27c65779db4e9d6e96f9a85b0096f10cd3c310aa
Mohammed Olaiya <mohammedolaiya@gmail.com>
2021-08-16 23:00:34 -0400

    Update README.java

--- README.java
+++ README.java
@@ -80,4 +80,10 @@ public static void main(String[] args) {
     System.out.println("Total number of ATMs: " + ATM.numATMs); 
     ATM firstATM = new ATM(1000);
     ATM secondATM = new ATM(500);
-    System.out.println("Total number of ATMs: " + ATM.numATMs); 
+    System.out.println("Total number of ATMs: " + ATM.numATMs);
+    
+    System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);  
+    firstATM.withdrawMoney(500);
+    secondATM.withdrawMoney(200);
+    System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney); 
+  
